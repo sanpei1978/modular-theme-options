b27b2bcd15611d6aeeef9e83382d07780c91a2e5
Takuma Yamanaka <archer@sanpeity.com>
2016-10-17 10:43:32 +0900

    A little refactoring.

--- options/frontends/class-frontend.php
+++ options/frontends/class-frontend.php
@@ -2,16 +2,18 @@
 
 namespace FrontEnd;
 
+use \ThemeOptions\Config;
+
 require_once 'class-material.php';
 require_once 'class-bootstrap.php';
 require_once \ThemeOptions\LIB_PATH . '/class-config.php';
 
-if ( 'material' === \ThemeOptions\Config::get( 'frontend' ) ) {
+if ( 'material' === Config::get( 'frontend' ) ) {
 	class Front_End extends Material_Ui_Lite {
 		// Use base class.
 	}
 }
-if ( 'bootstrap' === \ThemeOptions\Config::get( 'frontend' ) ) {
+if ( 'bootstrap' === Config::get( 'frontend' ) ) {
 	class Front_End extends Bootstrap_4 {
 		// Use base class.
 	}
