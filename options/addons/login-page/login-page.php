180afd865a533a2c1138d177255b07e1f4e42a63
Takuma Yamanaka <archer@sanpeity.com>
2016-10-22 00:05:07 +0900

    Write Copyright.

--- options/addons/login-page/login-page.php
+++ options/addons/login-page/login-page.php
@@ -1,14 +1,21 @@
 <?php
-/*
-Plugin Name: Modular Theme Options
-Author: Takuma Yamanaka
-Plugin URI:
-Description: A framework designed to facilitate the development of WordPress Themes options.
-Version: 0.4.0
-Author URI: https://github.com/sanpei1978
-Domain Path: /languages
-Text Domain: theme-options
-*/
+/**
+ * Copyright (c) 2016 sanpeity (https://github.com/sanpei1978)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 namespace ThemeOptions\Addon;
 
